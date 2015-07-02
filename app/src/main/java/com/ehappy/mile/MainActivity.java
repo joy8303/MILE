39848d397393a65e896b468e609f8c61a209373b
joy <joy@localhost.localdomain>
2015-07-03 00:08:55 +0800

    Mile

--- app/src/main/java/com/ehappy/mile/MainActivity.java
+++ app/src/main/java/com/ehappy/mile/MainActivity.java
@@ -8,28 +8,35 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
     private EditText edtMile;
     private TextView txtKm;
-    private Button btnTran;
+    private Button btnTran, toastButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         edtMile=(EditText)findViewById(R.id.edtMile);
         txtKm=(TextView)findViewById(R.id.txtKm);
         btnTran=(Button)findViewById(R.id.btnTran);
+        toastButton = (Button) findViewById(R.id.btnTran);
 
         btnTran.setOnClickListener(btnTranListener);
+        toastButton.setOnClickListener(btnTranListener);
     }
     //自訂義方法 觸發按鈕後啟動功能
     private Button.OnClickListener btnTranListener = new Button.OnClickListener() {
         public void onClick(View v) {
+
             double miles=Double.parseDouble(edtMile.getText().toString());
             double km=1.61*miles;
-            txtKm.setText("時數=" + km+"公里");
+            txtKm.setText("時速=" + km+"公里");
+            if (miles==0 || miles<0)
+            Toast.makeText(v.getContext(), "顯示錯誤", Toast.LENGTH_LONG).show();
         }
     };
     @Override
